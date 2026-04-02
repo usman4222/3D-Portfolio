74fd870e5b815e8c8f9be490ff37c380c6f729ee
Usman4222 <usman4243ch@gmail.com>
2026-04-02 17:37:41 +0500

    remove import error

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,15 +1,10 @@
-import { Link } from "react-router-dom";
 import { config } from "../config";
 import "./styles/CallToAction.css";
 
 const CallToAction = () => {
   return (
     <div className="cta-section">
-      <div className="cta-buttons">
-        {/* <Link to="/play" className="cta-btn cta-btn-play" data-cursor="disable">
-          Play With Me →
-        </Link> */}
-        
+      <div className="cta-buttons">        
         <a 
           href={config.contact.linkedin} 
           target="_blank" 
